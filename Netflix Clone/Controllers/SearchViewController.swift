470ddf68c26e35a3df37fc9ba7267dc9f8925f2c
Mortuza Hossain <mortuzahossain1997@gmail.com>
2022-05-29 14:00:01 +0600

    Initial Commit

---
+++ Netflix Clone/Controllers/SearchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchViewController.swift
+//  Netflix Clone
+//
+//  Created by Mac Pc on 5/29/22.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+    
+
+}
---
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  UpcomingViewController.swift
+//  Netflix Clone
+//
+//  Created by Mac Pc on 5/29/22.
+//
+
+import UIKit
+
+class UpcomingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+    
+
+    
+
+}
